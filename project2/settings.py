a9ce3a1bac9e8c2f57b8627ba9bfddc7cb41748e
Damião Ribeiro <supredes@hotmail.com>
2022-04-16 22:28:41 -0300

    Atualizadooo

--- project2/settings.py
+++ project2/settings.py
@@ -21,7 +21,6 @@ from pathlib import Path
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 #BASE_DIR = Path(__file__).resolve().parent.parent
